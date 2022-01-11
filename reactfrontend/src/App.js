e80717caa96e8269e50ff8232519abda0a131e73
conant2 <61479615+conant2@users.noreply.github.com>
2022-01-11 12:05:20 -0500

    Search Bar/banner edits

--- reactfrontend/src/App.js
+++ reactfrontend/src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import Banner from './Banner';
 import './App.css'
-
+const posts = [
+  { id: '1', name: 'This first post is about React' },
+  { id: '2', name: 'This next post is about Preact' },
+  { id: '3', name: 'We have yet another React post!' },
+  { id: '4', name: 'This is the fourth and final post' },
+];
 function App() {
   return (
     <div>
       <Banner />
-      <Banner />
+      <ul>
+          {posts.map((post) => (
+              <li key={post.id}>{post.name}</li>
+          ))}
+       </ul>
     </div>
     
   )
